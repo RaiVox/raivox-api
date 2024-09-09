b1f2ea7f3aa2fcaa10e4059db5538bc38f0ba119
owellandry <owellandry10@gmail.com>
2024-09-08 22:17:29 -0500

    Update changes

--- src/config/sequelize.ts
+++
@@ -1,9 +0,0 @@
-// src/config/sequelize.ts
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('raivox', 'root', '0001', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
-
-export default sequelize;
